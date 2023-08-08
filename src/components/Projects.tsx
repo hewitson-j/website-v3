7e3787d4930908efc7b62b71919be2c590a6ecd2
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-08-07 20:27:26 -0600

    Added selector color and project responsive spacing.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,23 @@
-import { Button, Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import MediaCard from "./MediaCard";
 
-export default function Projects(){
-    return <>
-    
-    <Grid container spacing={3}>
-        <Grid item xs={4}><MediaCard id={1}/></Grid>
-        <Grid item xs={4}><MediaCard id={2}/></Grid>
-        <Grid item xs={4}><MediaCard id={3}/></Grid>
-        <Grid item xs={4}><MediaCard id={4}/></Grid>
-    </Grid>
-    <br/>
-    <Container>
-      <Button variant='contained' size='large'>Project Description</Button>
-    </Container>
-    
-  </>
+export default function Projects() {
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={4}>
+          <MediaCard id={1} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <MediaCard id={2} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <MediaCard id={3} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <MediaCard id={4} />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
